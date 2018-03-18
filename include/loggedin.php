5daae3bd3f5bf59693a200e5637fbb9bee29a80f
Taymor <tayhum@gmail.com>
2018-03-18 01:28:03 +0100

    expanding admin menu

--- include/loggedin.php
+++ include/loggedin.php
@@ -2,4 +2,6 @@
   <li><a href="include/logout.php">Logout</a></li>
   <li><a href="index.php?page=include/editnews">Redigere Nyheder</a></li>
   <li><a href="index.php?page=include/editnews">Administration</a></li>
+  <li><a href="index.php?page=include/editnews">Administration</a></li>
+  <li><a href="index.php?page=include/editnews">Administration</a></li>
 </ul>
